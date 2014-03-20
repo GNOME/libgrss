876a8327d6ded380ff5e6c5130c0e00e543eba24
Roberto Guido <bob4job@gmail.com>
2014-03-20 02:15:21 +0100

    Updated autotools files

--- autogen.sh
+++ autogen.sh
@@ -5,10 +5,19 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="libgrss"
+REQUIRED_AUTOMAKE_VERSION=1.9
+REQUIRED_M4MACROS=introspection.m4
 
-which gnome-autogen.sh || {
-        echo "Missing gnome-autogen.sh: you need to install gnome-common"
-        exit 1
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/$PKG_NAME.doap) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
 }
 
-USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common."
+    exit 1
+}
+
+. gnome-autogen.sh
